855d135a9941e4b16cbc27a6cf07bc6e7567f73c
HackerManOSU <130389151+HackerManOSU@users.noreply.github.com>
2024-06-14 23:06:24 +0000

    Slick Lib + About Me Carousel

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Smooth Scroll Behavior
-
+// Smooth Scroll Behavior on link click
 document.addEventListener('DOMContentLoaded', function() {
     // Select all links with hashes
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -21,9 +20,30 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// Stopping arrow bounce
 
+// Stopping arrow bounce
 function stopBouncing() {
     var element = document.getElementById("arrow-icon");
     element.classList.remove("fa-bounce");
-  } 
+}
+
+// Function to dynamically resize SVG elements based on viewport width
+function resizeSVGs() {
+    // Get all SVG elements within the container with the class 'images'
+    const svgs = document.querySelectorAll('.svg-icons');
+
+    // Determine new size based on viewport width
+    const newSize = window.innerWidth > 700 ? '50px' : '30px';
+
+    // Loop through each SVG and set its width and height to newSize
+    svgs.forEach(svg => {
+        svg.style.width = newSize;
+        svg.style.height = newSize;
+    });
+}
+
+// Add event listener to resize SVGs on window resize
+window.addEventListener('resize', resizeSVGs);
+
+// Call function on initial load
+resizeSVGs();
---
+++ carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    });
+  });
+  
+  // Slick version 1.5.8
