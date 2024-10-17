9e352f4e752821cfb322a0ee3d617ef98e614293
HackerManOSU <130389151+HackerManOSU@users.noreply.github.com>
2024-10-17 21:24:06 +0000

    My photos + cursor hover fixes 🏗️

--- Personal-Portfolio/src/components/Me/Me.tsx
+++ Personal-Portfolio/src/components/Me/Me.tsx
@@ -8,7 +8,7 @@ const Me: React.FC = () => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
       triggerOnce: true,
-      threshold: 0.1,
+      threshold: 0.01,
   });
 
   const containerVariants = {
@@ -46,7 +46,7 @@ const Me: React.FC = () => {
     <div id='me' className='border-t-4 border-white text-white pt-12 pb-8 flex flex-col items-center'>
       <h1 className='text-6xl font-bold'>Me!</h1>
 
-      <div className='py-8'>
+      <div className='py-8 text-center mx-12'>
 
         <p>I study <strong>Computer Science</strong> at <strong>Oregon State University</strong> with a focus in Human-Computer-Interaction and Cybersecurtity</p>
 
@@ -60,64 +60,87 @@ const Me: React.FC = () => {
                 className='flex flex-col items-center text-4xl'
             >
 
-          <div className='py-8 text-6xl font-bold'>I like...</div>
+          <motion.div variants={titleVariants} className='py-8 text-5xl font-bold'>My Life in Photos</motion.div>
 
           <div className='flex flex-col items-center justify-center'>
 
-            <motion.div variants={titleVariants} className='font-bold'>Exploring</motion.div>
+            <motion.div variants={photoVariants} className='w-[100%] grid grid-cols-1 md:grid-cols-3 gap-4 p-4 place-items-center'>
 
-            <motion.div variants={photoVariants} className='w-[100%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 place-items-center'>
 
-              <img className='img-me' src="/Images/Beach.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach4.jpeg" alt="" />
+              <img className='img-me' src="/Images/Dirtbike.jpeg" alt="" />
+              <img className='img-me' src="/Images/Lighthouse2.jpeg" alt="" />
               <img className='img-me' src="/Images/Waterfall.jpeg" alt="" />
-              <img className='img-me' src="/Images/Beach2.jpeg" alt="" />
-              <img className='img-me' src="/Images/Horse.jpeg" alt="" />
-              <img className='img-me' src="/Images/Waterfall1.jpeg" alt="" />
               <img className='img-me' src="/Images/Jamaica.jpeg" alt="" />
-              <img className='img-me' src="/Images/Lh.jpeg" alt="" />
-              <img className='img-me' src="/Images/Horse2.jpeg" alt="" />
-              <img className='img-me' src="/Images/Horse3.jpeg" alt="" />
-              <img className='img-me' src="/Images/Progreso.jpeg" alt="" />
-              <img className='img-me' src="/Images/Snow2.jpeg" alt="" />
-              <img className='img-me' src="/Images/Stingray.jpeg" alt="" />
-              <img className='img-me' src="/Images/.jpeg" alt="" />
-
-            </motion.div>
-            
-          </div>
-
-
-          <div className='flex flex-col items-center justify-center'>
-
-            <motion.div variants={titleVariants}>Friends and Family</motion.div>
-
-            <motion.div variants={photoVariants} className='w-[100%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 place-items-center'>
-
-              <img className='img-me' src="/Images/Em3.jpeg" alt="" />
-              <img className='img-me' src="/Images/UO.jpeg" alt="" />
-              <img className='img-me' src="/Images/Wedding.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Graduation.jpeg" alt="" />
               <img className='img-me' src="/Images/Skagway.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fish2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Horse3.jpeg" alt="" />
+              <img className='img-me' src="/Images/Em3.jpeg" alt="" />
+              <img className='img-me' src="/Images/Flower1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Family2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Em2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Flower2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Mechanic1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Family1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach5.jpeg" alt="" />
+              <img className='img-me' src="/Images/COJO2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Em.jpeg" alt="" />
+              <img className='img-me' src="/Images/Football.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach3.jpeg" alt="" />
+              <img className='img-me' src="/Images/Cap.jpeg" alt="" />
+              <img className='img-me' src="/Images/Alligator.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fish1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Horse2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Progreso.jpeg" alt="" />
+              <img className='img-me' src="/Images/Heisman.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fish3.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach6.jpeg" alt="" />
+              <img className='img-me' src="/Images/Waterfall1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Childers.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fish6.jpeg" alt="" />
+              <img className='img-me' src="/Images/Horseback.jpeg" alt="" />
+              <img className='img-me' src="/Images/Boat.jpeg" alt="" />
+              <img className='img-me' src="/Images/Wedding.jpeg" alt="" />
+              <img className='img-me' src="/Images/Ice.jpeg" alt="" />
+              <img className='img-me' src="/Images/Milo1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fish5.jpeg" alt="" />
+              <img className='img-me' src="/Images/Lh.jpeg" alt="" />
+              <img className='img-me' src="/Images/Snow2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Beach7.jpeg" alt="" />
+              <img className='img-me' src="/Images/Bear.jpeg" alt="" />
+              <img className='img-me' src="/Images/Stingray.jpeg" alt="" />
+              <img className='img-me' src="/Images/UO.jpeg" alt="" />
+              <img className='img-me' src="/Images/Horse.jpeg" alt="" />
+              <img className='img-me' src="/Images/Snow.jpeg" alt="" />
+              <img className='img-me' src="/Images/Milo2.jpeg" alt="" />
+              <img className='img-me' src="/Images/Bend1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Fair1.jpeg" alt="" />
 
             </motion.div>
             
           </div>
+
 
           <div className='flex flex-col items-center justify-center'>
 
             <motion.div variants={titleVariants}>My Hobbies</motion.div>
 
-            <motion.div variants={photoVariants} className='w-[100%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 place-items-center'>
+            <motion.div variants={photoVariants} className='w-[100%] grid grid-cols-1 md:grid-cols-3 gap-4 p-4 place-items-center' >
 
-              <img className='img-me-top' src="/Images/Guitar.jpeg" alt="" />
+              <img className='img-me' src="/Images/Guitar.jpeg" alt="" />
               <img className='img-me' src="/Images/Piano.jpeg" alt="" />
-              <img className='img-me' src="/Images/Fish1.jpeg" alt="" />
-              <img className='img-me' src="/Images/Fish2.jpeg" alt="" />
-              <img className='img-me' src="/Images/Fish3.jpeg" alt="" />
+              <img className='img-me' src="/Images/Dirtbike1.jpeg" alt="" />
+              <img className='img-me' src="/Images/Gun.jpeg" alt="" />
+              <img className='img-me' src="/Images/Ninja.jpeg" alt="" />
               <img className='img-me' src="/Images/Fish4.jpeg" alt="" />
-              <img className='img-me' src="/Images/Fish5.jpeg" alt="" />
+              <img className='img-me' src="/Images/Mechanic1.jpeg" alt="" />
+
 
             </motion.div>
-
+            
           </div>
 
           <div>
